ec1a3ea62effca15714d5102a4a75e1201d73fe5
Logan <hotzlogan@gmail.com>
2020-10-22 13:53:04 -0500

    more verbose syntax error; added ModelIdentification

--- pynare/errors/__init__.py
+++ pynare/errors/__init__.py
@@ -1,4 +1,6 @@
-""" pynare errors """
+""" 
+pynare errors 
+"""
 
 class PynareError(Exception):
 	pass
@@ -14,7 +16,30 @@ class PynareWarning(object):
 
 
 class PynareSyntaxError(PynareError):
-	def __init__(self, line, pos):
-		self.line = line
-		self.pos = pos
-		super().__init__()
+	def __init__(self, token):
+		msg = (
+			f'syntax error: {repr(token.value)} in line '
+			f'{token.line_number} in column {token.line_pos}'
+		)
+		super().__init__(msg)
+
+
+class ModelIdentificationError(PynareError):
+	def __init__(self, bk_type=None, n0=None, n1=None):
+
+		if bk_type == 'order':
+			msg = (
+				'model does not satisfy Blanchard-Kahn (1980) order condition: '
+				f'{n0} explosive eigenvalues and {n1} forward-facing variables'
+			)
+
+		elif bk_type == 'rank':
+			msg = 'model does not satisfy Blanchard-Kahn (1980) rank condition'
+
+		else:
+			msg = (
+				f'rank condition not met: model has rank {n0} '
+				f'and there are {n1} static variables'
+			)
+			
+		super().__init__(msg)
